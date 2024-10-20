8c3c003b44c1f59f21c90483694726b254d3dd9f
DmiTsa <dmitrytaskaev.dt@gmail.com>
2024-10-20 09:16:31 +0300

    feat: some functions

--- js/classes.js
+++ js/classes.js
@@ -1,34 +1,70 @@
+class diceSet {
+    constructor(attempt) {
+        this.attempt = attempt
+        this.state = [0, 0, 0, 0, 0]
+        this.blocked = [false, false, false, false, false]
+        this.max = 6
+    }
+
+    togglBblock(n) {
+        if (this.state[n] !== 0) {
+            this.blocked[n] = !this.blocked[n]
+        }
+    }
+
+    setRandom() {
+        if (this.attempt) {
+            this.blocked.forEach((el, i) => {
+                if (!el) { this.state[i] = Math.floor(Math.random() * this.max + 1) }
+            })
+        }
+        this.attempt--
+    }
+
+    getSet() {
+        return this.state
+    }
+
+    // decAttempt() {
+    //     this.attempt = this.attempt - 1
+    // }
+
+}
+
 class userTable {
-    constructor() {
+    constructor(userName) {
+        this.total = 0
+        this.userName = userName
         this.state = {
             one: 0,
             isOne: true,
             two: 0,
             isTwo: true,
-            // three: 0,
+            three: 0,
             // isThree: true,
-            // four: 0,
+            four: 0,
             // isFour: true,
-            // five: 0,
+            five: 0,
             // isFive: true,
-            // six: 0,
+            six: 0,
             // isSix: true,
-            // kindThree: 0,
+            kindThree: 0,
             // isKindThree: true,
-            // kindFour: 0,
+            kindFour: 0,
             // isKindFour: true,
-            // house: 0,
+            house: 0,
             // isHouse: true,
-            // small: 0,
+            small: 0,
             // isSmall: true,
-            // large: 0,
+            large: 0,
             // isLarge: true,
-            // yat: 0,
+            yat: 0,
             // isYat: true,
             chance: 0,
             isChance: true
         }
     }
+
 
     setOne(arr) {
         const num = 1
@@ -59,8 +95,3 @@ class userTable {
 
     //totalScore
 }
-
-const a = new userTable()
-
-a.setChance([2, 3, 4])
-console.log(a.state);
---
+++ js/index.js
@@ -0,0 +1,205 @@
+// console.log('connect js');
+
+
+//#region VARIABELS
+
+const leaderboardNode = document.querySelector('.leaderboard')
+const gameWrapperNode = document.querySelector('.game-wrapper')
+const newGameNode = document.querySelector('.new_game')
+const newGameStartNode = document.querySelector('#new-game-start')
+const aboutNode = document.querySelector('.about')
+const menu = document.querySelector('#menu')
+
+const tablesNode = document.querySelector('.tables')
+const fieldSetNode = document.querySelector('.fieldset')
+
+const attemptButton = document.querySelector('#attempt')
+const stopButton = document.querySelector('#stop')
+
+let userTables = []
+let currentDiceSet
+// let currentAttempt
+
+//#endregion
+
+
+
+
+//#region FUNCTIONS
+
+function renderUserTable(userTable, destNode) {
+    // const [userName, one, two, three, four, five, six, kindThree, kindFour, house, small, large, yat, chance, total] = userTable.state
+
+    const table = document.createElement('div')
+    table.classList.add('table')
+
+    const player = document.createElement('div')
+    player.classList.add('player')
+    player.innerText = userTable.userName
+
+    const rows = document.createElement('div')
+    rows.classList.add('rows')
+
+    rows.innerHTML = `
+                        <div class="row">
+                            <img class="tileImg" src="./assets/field/one.png" alt="yatzy kind">
+                            <div class="kind_points">${userTable.state.one}</div>
+                        </div>
+
+                        <div class="row">
+                            <img class="tileImg" src="./assets/field/kind_three.png" alt="yatzy kind">
+                            <div class="kind_points">${userTable.state.kindThree}</div>
+                        </div>
+
+                        <div class="row">
+                            <img class="tileImg" src="./assets/field/two.png" alt="yatzy kind">
+                            <div class="kind_points">${userTable.state.two}</div>
+                        </div>
+
+                        <div class="row">
+                            <img class="tileImg" src="./assets/field/kind_four.png" alt="yatzy kind">
+                            <div class="kind_points">${userTable.state.kindFour}</div>
+                        </div>
+
+                        <div class="row">
+                            <img class="tileImg" src="./assets/field/three.png" alt="yatzy kind">
+                            <div class="kind_points">${userTable.state.three}</div>
+                        </div>
+
+                        <div class="row">
+                            <img class="tileImg" src="./assets/field/house.png" alt="yatzy kind">
+                            <div class="kind_points">${userTable.state.house}</div>
+                        </div>
+
+                        <div class="row">
+                            <img class="tileImg" src="./assets/field/four.png" alt="yatzy kind">
+                            <div class="kind_points">${userTable.state.four}</div>
+                        </div>
+
+                        <div class="row">
+                            <img class="tileImg" src="./assets/field/small.png" alt="yatzy kind">
+                            <div class="kind_points">${userTable.state.small}</div>
+                        </div>
+
+                        <div class="row">
+                            <img class="tileImg" src="./assets/field/five.png" alt="yatzy kind">
+                            <div class="kind_points">${userTable.state.five}</div>
+                        </div>
+
+                        <div class="row">
+                            <img class="tileImg" src="./assets/field/large.png" alt="yatzy kind">
+                            <div class="kind_points">${userTable.state.large}</div>
+                        </div>
+
+                        <div class="row">
+                            <img class="tileImg" src="./assets/field/six.png" alt="yatzy kind">
+                            <div class="kind_points">${userTable.state.six}</div>
+                        </div>
+
+                        <div class="row">
+                            <img class="tileImg" src="./assets/field/yat.png" alt="yatzy kind">
+                            <div class="kind_points">${userTable.state.yat}</div>
+                        </div>
+
+                        <div class="row">
+                            <img class="tileImg" src="./assets/field/chance.png" alt="yatzy kind">
+                            <div class="kind_points">${userTable.state.chance}</div>
+                        </div>
+
+                        <div class="row total">
+                            <img class="tileImg" src="./assets/score.png" alt="yatzy kind">
+                            <div class="kind_points points_score">${userTable.total}</div>
+                        </div>
+    `
+    table.appendChild(player)
+    table.appendChild(rows)
+
+    destNode.appendChild(table)
+}
+
+function renderDiceField(diceSet, destNode) {
+    const diceImgPathDefault = './assets/diceDefault.png'
+    const diceImgPaths = [
+        './assets/diceImg/d1.png',
+        './assets/diceImg/d2.png',
+        './assets/diceImg/d3.png',
+        './assets/diceImg/d4.png',
+        './assets/diceImg/d5.png',
+        './assets/diceImg/d6.png',
+    ]
+
+    destNode.innerHTML = ''
+    diceSet.state.forEach((el) => {
+        const node = document.createElement('img')
+        node.classList.add('dice')
+
+        if (el === 0) {
+            node.src = diceImgPathDefault
+        } else {
+            node.src = diceImgPaths[el - 1]
+        }
+
+        destNode.appendChild(node)
+    })
+}
+
+// [leaderboard game about]
+function pageRouter(pageName) {
+    if (pageName === 'leaderboard') {
+        leaderboardNode.classList.remove('hidden')
+        gameWrapperNode.classList.add('hidden')
+        aboutNode.classList.add('hidden')
+    } else if (pageName === 'game') {
+        gameWrapperNode.classList.remove('hidden')
+        aboutNode.classList.add('hidden')
+        leaderboardNode.classList.add('hidden')
+    } else if (pageName === 'about') {
+        aboutNode.classList.remove('hidden')
+        gameWrapperNode.classList.add('hidden')
+        leaderboardNode.classList.add('hidden')
+    }
+}
+
+//#endregion
+
+
+//#region LISTENERS
+
+//в будущем дописать запуск через диалоговое окно с настройками
+const playerNames = ['dima', 'gleb']
+// const playersQty = playerNames.length
+
+newGameStartNode.addEventListener('click', () => {
+    playerNames.forEach((el) => {
+        userTables.push(new userTable(el))
+    })
+
+    userTables.forEach((el) => {
+        renderUserTable(el, tablesNode)
+    })
+
+    currentDiceSet = new diceSet(3)
+    currentAttempt = currentDiceSet.attempt
+    renderDiceField(currentDiceSet, fieldSetNode)
+
+    newGameNode.classList.add('hidden')
+    pageRouter('game')
+})
+
+attemptButton.addEventListener('click', () => {
+    if (currentDiceSet.attempt) {
+        // console.log('att');
+        // console.log(currentDiceSet.attempt);
+
+        currentDiceSet.setRandom()
+        renderDiceField(currentDiceSet, fieldSetNode)
+    }
+})
+
+//listener click field
+
+menu.addEventListener('click', (e) => {
+    pageRouter(e.target.id)
+})
+
+//#endregion
